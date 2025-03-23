b3873f94c26cc671e747f9fea852573ce07e39f9
gmeeric <gurmeerg23@gmail.com>
2025-03-23 12:03:21 -0400

    Deleting Unnecessary Files
    These were junk files that were mainly just failed versions of the working files.

--- battleship.cpp
+++
@@ -1,8 +0,0 @@
-#include <fwwasm.h>
-
-auto main() -> int {
-    setBoardLED(1, 255, 255, 0, 1000, LEDManagerLEDMode::ledpulsefade);
-    waitms(1000);
-    exitToMainAppMenu();
-    return 0;
-}
--- stats.cpp
+++
@@ -1,8 +0,0 @@
-#include <fwwasm.h>
-
-auto main() -> int {
-    setBoardLED(2, 0, 255, 0, 1000, LEDManagerLEDMode::ledpulsefade);
-    waitms(1000);
-    exitToMainAppMenu();
-    return 0;
-}
